8057952fa74b6a2b1e3799e0fa7af25680320544
ccarper01 <ccarper01@ccarper01s-iMac.local>
2024-03-06 18:50:05 -0600

    createWidget

--- controllers/widgets.controller.js
+++ controllers/widgets.controller.js
@@ -2,3 +2,8 @@ export const getWidgets = async (req, res, next) => {
     console.log('Controller : getWidgets()');
     res.status(200).json([]);
 };
+
+export const createWidget = async (req, res, next) => {
+    console.log('Controller : createWidget()');
+    res.status(200).json({});
+};
--- routes/widgets.routes.js
+++ routes/widgets.routes.js
@@ -1,9 +1,12 @@
 import express from 'express';
-import { getWidgets } from '../controllers/widgets.controller.js';
+import { getWidgets, createWidget } from '../controllers/widgets.controller.js';
 
 const widgetsRouter = express.Router();
 
 // GET /api/v1/widgets
 widgetsRouter.get('/', getWidgets);
+
+// POST /api/v1/widgets
+widgetsRouter.post('/', createWidget);
 
 export default widgetsRouter;
