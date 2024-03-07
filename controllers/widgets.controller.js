b12974daf581fde45a4ba532228156abdc42ca01
ccarper01 <ccarper01@ccarper01s-iMac.local>
2024-03-06 18:58:46 -0600

    rest

--- controllers/widgets.controller.js
+++ controllers/widgets.controller.js
@@ -7,3 +7,23 @@ export const createWidget = async (req, res, next) => {
     console.log('Controller : createWidget()');
     res.status(200).json({});
 };
+
+export const getWidget = async (req, res, next) => {
+    console.log(`Controller : getWidget(${req.params.id})`);
+    res.status(200).json({});
+};
+
+export const replaceWidget = async (req, res, next) => {
+    console.log(`Controller : replaceWidget(${req.params.id})`);
+    res.status(200).json({});
+};
+
+export const deleteWidget = async (req, res, next) => {
+    console.log(`Controller : deleteWidget(${req.params.id})`);
+    res.status(200).json({});
+};
+
+export const updateWidget = async (req, res, next) => {
+    console.log(`Controller : updateWidget(${req.params.id})`);
+    res.status(200).json({});
+};
--- routes/widgets.routes.js
+++ routes/widgets.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getWidgets, createWidget } from '../controllers/widgets.controller.js';
+import { getWidgets, createWidget, getWidget, replaceWidget, deleteWidget, updateWidget } from '../controllers/widgets.controller.js';
 
 const widgetsRouter = express.Router();
 
@@ -8,5 +8,17 @@ widgetsRouter.get('/', getWidgets);
 
 // POST /api/v1/widgets
 widgetsRouter.post('/', createWidget);
+
+// GET /api/v1/widgets/<id>
+widgetsRouter.get('/:id', getWidget);
+
+// PUT /api/v1/widgets/<id>
+widgetsRouter.put('/:id', replaceWidget);
+
+// DELETE /api/v1/widgets/<id>
+widgetsRouter.delete('/:id', deleteWidget);
+
+// PATCH /api/v1/widgets
+widgetsRouter.patch('/:id', updateWidget);
 
 export default widgetsRouter;
