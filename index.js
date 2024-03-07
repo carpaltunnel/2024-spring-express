2d8b6cc6f638d566f76ddaed086ea75a81df0a0e
ccarper01 <ccarper01@ccarper01s-iMac.local>
2024-03-06 18:31:33 -0600

    initial

---
+++ index.js
@@ -0,0 +1,9 @@
+import { express } from 'express';
+
+// This is my express application
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Starting express application on port ${port}`);
+});
